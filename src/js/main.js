4a97213140a0658c155456fdbf64bfe90e425d66
Denis <denisgreshnaykov@gmail.com>
2022-08-28 16:41:40 +0500

    add modal and fix modal

--- src/js/main.js
+++ src/js/main.js
@@ -5,9 +5,9 @@ $("button").on("click", function () {
   $("div").eq(2).toggleClass("active");
 });
 
-$("div").click(function () {
+/*$("div").click(function () {
   console.log($(this).index());
-});
+});*/
 
 //console.log($("div").eq(2).find(".some"));
 
